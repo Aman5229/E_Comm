cb87b6bca5dfe7c296efbdea948015bd230a2934
Aman Patel <Amanpatel5229@gmail.com>
2024-03-18 17:41:26 +0530

    add more field to products and categories

--- base/models.py
+++ base/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class BaseModel(models.Model):
 
   uid = models.UUIDField(primary_key=True, editable=False)
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_add=True)
+  created_at = models.DateTimeField(auto_now=True)
+  updated_at = models.DateTimeField(auto_now_add=True)
    
   class Meta:
     abstract = True
--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -39,6 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+
+EXTERNAL_APP = [
+  'products'
+]
+
+INSTALLED_APPS +=EXTERNAL_APP
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
